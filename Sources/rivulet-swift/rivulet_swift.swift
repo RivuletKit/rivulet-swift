3f39571d64323222e9a713d5f4abec299f506294
PBK-B <pbk-b@PBK6.cn>
2024-02-17 12:13:50 +0800

    feat: add testExampleReply

--- Sources/rivulet-swift/rivulet_swift.swift
+++ Sources/rivulet-swift/rivulet_swift.swift
@@ -1,2 +1,58 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Protos
+import SwiftProtobuf
+
+// 类型别名
+typealias _RivuletRequest = Com_Rivuletkit_Common_Collection_Request
+typealias _RivuletResponse = Com_Rivuletkit_Common_Collection_Response
+
+// 处理接口
+protocol RivuletHandleInterface {
+    // 请求实现
+    func Reply(request: RivuletRequest) throws -> RivuletResponse?
+}
+
+// 上下文
+class RivuletContext {
+    internal var iRivuletHandle: RivuletHandleInterface
+
+    init(handle: RivuletHandleInterface) {
+        iRivuletHandle = handle
+    }
+}
+
+// 数据基类
+class RivuletBaseDataClass<T: SwiftProtobuf.Message> {
+    internal var context: RivuletContext
+    internal var instance: T
+
+    internal init(context: RivuletContext, instance: T) {
+        self.context = context
+        self.instance = instance
+    }
+
+    // json 解码
+    convenience init(context: RivuletContext, jsonString: String) throws {
+        self.init(
+            context: context,
+            instance: try T(jsonString: jsonString)
+        )
+    }
+
+    // json 编码
+    func jsonString() throws -> String {
+        return try instance.jsonString()
+    }
+}
+
+// Request 类
+class RivuletRequest: RivuletBaseDataClass<_RivuletRequest> {
+    public func Reply() throws -> RivuletResponse? {
+        return try context.iRivuletHandle.Reply(request: self)
+    }
+}
+
+// Response 类
+class RivuletResponse: RivuletBaseDataClass<_RivuletResponse> {}
--- Tests/RivuletSwiftTests/RivuletSwiftTests.swift
+++ Tests/RivuletSwiftTests/RivuletSwiftTests.swift
@@ -14,9 +14,19 @@ final class RivuletSwiftTests: XCTestCase {
         var col = Com_Rivuletkit_Common_Collection_Collection()
         col.info = Com_Rivuletkit_Common_Collection_CollectionInfo()
         col.info.name = "hello"
-        let jsonStr = try col.jsonString()
 
-        Swift.print("[Echo] col to json: \(jsonStr)")
+        var request = Com_Rivuletkit_Common_Collection_Request()
+        var url = Com_Rivuletkit_Common_Collection_URL()
+        url.protocol = "https"
+        url.host = "bin.zmide.com"
+        url.path = "/feed"
+        url.querys = []
+        request.url = url
+        request.method = "GET"
+        request.headers = []
+
+        let jsonStr = try request.jsonString()
+        Swift.print("[Echo] request to json: \(jsonStr)")
     }
 
     func testExampleJsonToCollection() throws {
@@ -30,5 +40,30 @@ final class RivuletSwiftTests: XCTestCase {
         let col = try Com_Rivuletkit_Common_Collection_Collection(jsonString: jsonStr)
 
         Swift.print("[Echo] col name is: \(col.info.name)")
+    }
+
+    func testExampleReply() throws {
+        // 实现接口
+        struct InHandle: RivuletHandleInterface {
+            func Reply(request: RivuletSwift.RivuletRequest) throws -> RivuletSwift.RivuletResponse? {
+                Swift.print("[InHandle] request <\(request.instance.url.host)> Reply loading…")
+                return nil
+            }
+        }
+
+        // 构造上下文
+        let context = RivuletContext(handle: InHandle())
+
+        // 导入请求
+        // let jsonStr = "{\"info\":{\"name\":\"hello\"}}"
+        let jsonStr = "{\"url\":{\"protocol\":\"https\",\"host\":\"bin.zmide.com\",\"path\":\"/feed\"},\"method\":\"GET\"}"
+        let request = try RivuletRequest(context: context, jsonString: jsonStr)
+
+        // 发送请求
+        if let response = try request.Reply() {
+            Swift.print("[Reply] request <\(response.instance.code)> done…")
+        } else {
+            Swift.print("[Reply] request error…")
+        }
     }
 }
